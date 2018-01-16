b767b67e8eb1b474cf212acf57de96b2127b862e
aishwaryajohn <35484294+aishwaryajohn@users.noreply.github.com>
2018-01-16 15:18:39 +0530

    Add files via upload

---
+++ divisionn.py
@@ -0,0 +1,36 @@
+Python 3.6.3 (default, Oct  3 2017, 21:45:48) 
+[GCC 7.2.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+=========== RESTART: /home/ec2017b111/aishwaryajohn/1st program.py ===========
+>>> 
+>>> 
+=========== RESTART: /home/ec2017b111/aishwaryajohn/1st program.py ===========
+Traceback (most recent call last):
+  File "/home/ec2017b111/aishwaryajohn/1st program.py", line 2, in <module>
+    Print("aishwaryajohn")
+NameError: name 'Print' is not defined
+>>> 
+=========== RESTART: /home/ec2017b111/aishwaryajohn/1st program.py ===========
+aishwaryajohn
+>>> print("xyz")
+xyz
+>>> 
+============ RESTART: /home/ec2017b111/aishwaryajohn/addition.py ============
+30
+>>> 
+============ RESTART: /home/ec2017b111/aishwaryajohn/addition.py ============
+addition of 10 and, 20 is, 30
+>>> 
+============== RESTART: /home/ec2017b111/aishwaryajohn/addy.py ==============
+addition of 100 and, 200 ,is 300
+>>> 
+=============== RESTART: /home/ec2017b111/aishwaryajohn/sub.py ===============
+subtraction of  20 and, 10 is, 10
+>>> 
+============== RESTART: /home/ec2017b111/aishwaryajohn/multi.py ==============
+multiplication of 10 and, 20 is, 200
+>>> 
+============== RESTART: /home/ec2017b111/aishwaryajohn/power.py ==============
+2.0
+>>> 
