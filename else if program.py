0a4c36418cba93876f7607324de0fb63cdc2382c
aishwaryajohn <35484294+aishwaryajohn@users.noreply.github.com>
2018-01-24 12:22:44 +0530

    Add files via upload

---
+++ else if program.py
@@ -0,0 +1,11 @@
+a=int(input("enter marks"))
+if a > 90:
+    print("given grade number is A")
+elif a > 80:
+    print("grade number is B")
+elif a > 70:
+    print("grade number is C")
+elif a > 60:
+    print("grade number is D")
+elif a > 50:
+    print("grade is fail")
---
+++ if else program.py
@@ -0,0 +1,5 @@
+a= int(10)
+if a > 0:
+    print("given number is positive")
+else:
+    print("given number is negative")
