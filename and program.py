1a2c45fc328fc1f5f956887b49fed589a57ae37a
aishwaryajohn <35484294+aishwaryajohn@users.noreply.github.com>
2018-01-31 11:40:47 +0530

    Add files via upload

---
+++ and program.py
@@ -0,0 +1,21 @@
+Python 3.6.3 (default, Oct  3 2017, 21:45:48) 
+[GCC 7.2.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+======= RESTART: /home/ec2017b111/aishwaryajohn/shorthand operators.py =======
+>>> 
+================== RESTART: /home/ec2017b111/and program.py ==================
+hi
+>>> 1==1 or 2!==2
+SyntaxError: invalid syntax
+>>> 1==1 and 2==2
+True
+>>> 1==1 and 2==2
+True
+>>> 1==1 or 2==2
+True
+>>> 
+============ RESTART: /home/ec2017b111/aishwaryajohn/program 4.py ============
+8
+4
+>>> 
---
+++ program 4.py
@@ -0,0 +1,7 @@
+n=6
+if n<8:
+    print("8")
+if n>7:
+    print("7")
+if n>4:
+    print("4")
